468a00305c36b4684fa88a3075fe75586a60eb64
bailanxiaobailv <1327225488@qq.com>
2024-07-12 15:14:56 +0800

    master

---
+++ 23.12.24/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include "max.h"//引用自己的头文件
+int main()
+{
+    int a=10;
+    int b=20;
+    int c=Max(a,b);
+    printf("hello world");
+    cout<<c<<endl;
+    return 0;
+}
